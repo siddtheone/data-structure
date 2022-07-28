c35e606840b2926947bf61190d2b7b998f6cf75a
siddtheone <45398971+siddtheone@users.noreply.github.com>
2022-07-28 19:13:26 +0530

    Adding tests for depth first

--- src/tree-traversal/depth-first.test.js
+++ src/tree-traversal/depth-first.test.js
@@ -7,6 +7,7 @@ import {
 
 it("traverse tree with pre order, depth first", () => {
   const tree = new BinarySearchTree();
+  expect(depthFirstPostOrder(tree)).toEqual([null]);
   [10, 6, 15, 3, 8, 20].forEach((node) => tree.insert(node));
   console.log(tree);
   expect(depthFirstPreOrder(tree)).toEqual([10, 6, 3, 8, 15, 20]);
@@ -14,12 +15,14 @@ it("traverse tree with pre order, depth first", () => {
 
 it("traverse tree with in order, depth first", () => {
   const tree = new BinarySearchTree();
+  expect(depthFirstPostOrder(tree)).toEqual([null]);
   [10, 6, 15, 3, 8, 20].forEach((node) => tree.insert(node));
   expect(depthFirstInOrder(tree)).toEqual([3, 6, 8, 10, 15, 20]);
 });
 
 it("traverse tree with post order, depth first", () => {
   const tree = new BinarySearchTree();
+  expect(depthFirstPostOrder(tree)).toEqual([null]);
   [10, 6, 15, 3, 8, 20].forEach((node) => tree.insert(node));
   expect(depthFirstPostOrder(tree)).toEqual([3, 8, 6, 20, 15, 10]);
 });
