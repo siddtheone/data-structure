aca63a187fc6ee050764e33d71c3d27b1fa95ebb
S <siddtheone@gmail.com>
2024-08-03 11:36:29 +0530

    DFS updates

--- src/tree-traversal/depth-first.js
+++ src/tree-traversal/depth-first.js
@@ -1,37 +1,31 @@
 export function depthFirstPreOrder(tree) {
-  if (tree.root === null) return [null];
-
   const nodes = [];
   function preOrder(node) {
-    nodes.push(node.value);
-    if (node.left) preOrder(node.left);
-    if (node.right) preOrder(node.right);
+    node?.value && nodes.push(node.value);
+    node?.left && preOrder(node.left);
+    node?.right && preOrder(node.right);
   }
   preOrder(tree.root);
 
   return nodes;
 }
 export function depthFirstInOrder(tree) {
-  if (tree.root === null) return [null];
-
   const nodes = [];
   function inOrder(node) {
-    if (node.left) inOrder(node.left);
-    nodes.push(node.value);
-    if (node.right) inOrder(node.right);
+    node?.left && inOrder(node.left);
+    node?.value && nodes.push(node.value);
+    node?.right && inOrder(node.right);
   }
   inOrder(tree.root);
 
   return nodes;
 }
 export function depthFirstPostOrder(tree) {
-  if (tree.root === null) return [null];
-
   const nodes = [];
   function postOrder(node) {
-    if (node.left) postOrder(node.left);
-    if (node.right) postOrder(node.right);
-    nodes.push(node.value);
+    node?.left && postOrder(node.left);
+    node?.right && postOrder(node.right);
+    node?.value && nodes.push(node.value);
   }
   postOrder(tree.root);
 
--- src/tree-traversal/depth-first.test.js
+++ src/tree-traversal/depth-first.test.js
@@ -2,26 +2,26 @@ import { BinarySearchTree } from "../binary-search-tree";
 import {
   depthFirstInOrder,
   depthFirstPostOrder,
-  depthFirstPreOrder
+  depthFirstPreOrder,
 } from "./depth-first";
 
 it("traverse tree with pre order, depth first", () => {
   const tree = new BinarySearchTree();
-  expect(depthFirstPostOrder(tree)).toEqual([null]);
+  expect(depthFirstPostOrder(tree)).toEqual([]);
   [10, 6, 15, 3, 8, 20].forEach((node) => tree.insert(node));
   expect(depthFirstPreOrder(tree)).toEqual([10, 6, 3, 8, 15, 20]);
 });
 
 it("traverse tree with in order, depth first", () => {
   const tree = new BinarySearchTree();
-  expect(depthFirstPostOrder(tree)).toEqual([null]);
+  expect(depthFirstPostOrder(tree)).toEqual([]);
   [10, 6, 15, 3, 8, 20].forEach((node) => tree.insert(node));
   expect(depthFirstInOrder(tree)).toEqual([3, 6, 8, 10, 15, 20]);
 });
 
 it("traverse tree with post order, depth first", () => {
   const tree = new BinarySearchTree();
-  expect(depthFirstPostOrder(tree)).toEqual([null]);
+  expect(depthFirstPostOrder(tree)).toEqual([]);
   [10, 6, 15, 3, 8, 20].forEach((node) => tree.insert(node));
   expect(depthFirstPostOrder(tree)).toEqual([3, 8, 6, 20, 15, 10]);
 });
